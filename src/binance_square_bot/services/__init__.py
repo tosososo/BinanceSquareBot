ba35dd45e7f47a84e4b3f72be494627b78e85631
fuyang <854771076@qq.com>
2026-04-21 00:57:27 +0800

    feat: clean up old files, complete refactoring

--- src/binance_square_bot/services/__init__.py
+++ src/binance_square_bot/services/__init__.py
@@ -1,20 +1,15 @@
-from .binance_publisher import BinancePublisher
-from .generator import TweetGenerator
-from .polymarket_fetcher import PolymarketFetcher
-from .polymarket_filter import PolymarketFilter
-from .publisher import PublisherService
-from .research_generator import ResearchGenerator
-from .spider import FnSpiderService as ForesightNewsSpider
-from .storage import StorageService as Storage
+# src/binance_square_bot/services/__init__.py
+from .storage import StorageService
+from .source import FnSource, PolymarketSource
+from .target import BinanceTarget
+from .cli import FnCliService, PolymarketCliService, CommonCliService
 
 __all__ = [
-    "Storage",
-    "ForesightNewsSpider",
-    "TweetGenerator",
-    "PublisherService",
-    "BinancePublisher",
-    "PolymarketFetcher",
-    "PolymarketFilter",
-    "ResearchGenerator",
+    "StorageService",
+    "FnSource",
+    "PolymarketSource",
+    "BinanceTarget",
+    "FnCliService",
+    "PolymarketCliService",
+    "CommonCliService",
 ]
-
